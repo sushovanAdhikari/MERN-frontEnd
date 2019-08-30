f4c781331605e1d82757f29f02399f09ac8e9184
sushovanAdhikari <sushovan.adhikari@selu.edu>
2019-08-30 00:05:57 -0500

    Left out push
    Clean up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import React, { Component } from 'react';
-import {Navigation } from "react-mdl";
-// import { BrowserRouter as Router, Route} from 'react-router-dom';
-// import { SignIn  } from './components/pages/SignIn';
+import React, { Component } from "react";
+import { Navigation } from "react-mdl";
 import Routes from "./router";
 import { Link } from "react-router-dom";
-import './App.css';
-
-
+import "./App.css";
 
 class App extends Component {
-  
   logout = () => {
     console.log("Logging out...");
     localStorage.removeItem("Token");
@@ -17,63 +12,28 @@ class App extends Component {
   };
 
   render() {
-    
     const styles = {
-
       appStyle: {
-        display: 'flex',
-        justifyContent: 'center',
-        flexDirection: 'column'
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column"
       }
-    }
+    };
 
-      return ( 
-        //<Router>
-        <div className="App" style={styles.appStyle}>
-            {/* <Route exact path="/" render = { props => (
+    return (
+      <div className="App" style={styles.appStyle}>
+        {localStorage.getItem("Token") ? (
+          <Link to="/Dashboard"></Link>
+        ) : (
+          <Navigation>
+            <Link to="/"></Link>
+          </Navigation>
+        )}
 
-              <React.Fragment>
-              <SignIn  />
-              </React.Fragment>
-              )} 
-              />   */}
-
-              {/* <Route exact path="/dashboard" render = { props => (
-
-              <React.Fragment>
-                 if (localStorage.getItem("Token")) {
-                  <Dashboard />
-                } else {
-                  alert("Error")
-                }
-                
-                
-              </React.Fragment>
-              )}  */}
-
-{/* 
-              <Navigation>
-              <Link to="/Dashboard"></Link>
-            </Navigation> */}
-
-          {localStorage.getItem("Token") ? (
-
-              <Link to="/Dashboard"></Link>
-            
-            ) : (
-            <Navigation>
-              <Link to="/"></Link>
-            </Navigation>
-          )}
-
-
-            <div className="page-content" />
-            <Routes />
-
-
-
-          </div>
-      )
+        <div className="page-content" />
+        <Routes />
+      </div>
+    );
   }
 }
 
